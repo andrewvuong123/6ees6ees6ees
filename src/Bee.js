e45d39cfde78b75d0f084fe34ccdd8f7aab22860
Andrew Vuong <andrew.vuong@berkeley.edu>
2020-08-24 11:04:58 -0700

    Implemented the Grub class and its subclasses using the ES6 style pattern

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
-class Bee {
-  // TODO..
+// extend keyword used to set the class as a subclass
+class Bee extends Grub {
+  constructor() {
+    // super keyword used as a function to call parent class with the params passed into this subclass (must call before using this)
+    super(arguments);
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
 };
